22b7ccaaeb3771ad10f8ac192700c4f100d55036
jigcau89 <jigcau89@hotmail.com>
2022-05-11 20:32:55 -0300

    version 1

---
+++ src/test/java/com/busCAR/busCAR/BusCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busCAR.busCAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
